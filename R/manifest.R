cd427fa610f0ca7eac75c4de1fd72934a45baed3
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-09-15 21:49:05 +0000

    Make manifest make more sense

--- R/manifest.R
+++ R/manifest.R
@@ -1,26 +1,5 @@
-.projr_build_manifest_save <- function(manifest, output_run) {
-  yml_projr_dir <- projr_yml_get()[["directories"]]
-  label_vec <- names(yml_projr_dir)[
-    grepl("^archive|^output", .projr_dir_label_strip(names(yml_projr_dir)))
-  ]
-  for (x in label_vec) {
-    if ((!output_run) && grepl("^archive", .projr_dir_label_strip(x))) {
-      next
-    }
-    if ("manifest" %in% names(yml_projr_dir[[x]])) {
-      if (!yml_projr_dir[[x]][["manifest"]]) {
-        next
-      }
-    }
-    dir_save <- projr_dir_get(x, output_safe = !output_run)
-    saveRDS(manifest, file.path(dir_save, "manifest.rds"))
-    utils::write.csv(manifest, file.path(dir_save, "manifest.csv"))
-  }
-  invisible(TRUE)
-}
-
 # hashed beforehand
-.projr_build_manifest_hash_pre <- function() {
+.projr_build_manifest_hash_pre <- function(output_run) {
   yml_projr_dir <- projr_yml_get()[["directories"]]
   cache_vec <- names(yml_projr_dir)[
     grepl("^cache", .projr_dir_label_strip(names(yml_projr_dir)))
@@ -41,11 +20,14 @@
       )
     )
   }
-  hash_tbl_list <- lapply(cache_vec, .projr_manifest_hash_label)
+  hash_tbl_list <- lapply(
+    cache_vec, .projr_manifest_hash_label,
+    output_run = output_run
+  )
   Reduce(rbind, hash_tbl_list)
 }
 
-.projr_build_manifest_hash_post <- function() {
+.projr_build_manifest_hash_post <- function(output_run) {
   yml_projr_dir <- projr_yml_get()[["directories"]]
   label_vec <- names(yml_projr_dir)[
     grepl(
@@ -78,14 +60,17 @@
       )
     )
   }
-  hash_tbl_list <- lapply(label_vec, .projr_manifest_hash_label)
+  hash_tbl_list <- lapply(
+    label_vec, .projr_manifest_hash_label,
+    output_run = output_run
+  )
   Reduce(rbind, hash_tbl_list)
 }
 
-.projr_manifest_hash_label <- function(label) {
+.projr_manifest_hash_label <- function(label, output_run) {
   # output is always in safe directory
   # as hashing is done before copying over to final directory
-  path_dir <- projr::projr_dir_get(label, output_safe = TRUE)
+  path_dir <- projr::projr_dir_get(label, output_safe = !output_run)
   fn_vec <- list.files(path_dir, full.names = TRUE, recursive = TRUE)
   if (length(fn_vec) == 0) {
     return(data.frame(
@@ -100,6 +85,36 @@
   data.frame(
     label = label,
     fn = fs::path_rel(fn_vec, path_dir) |> as.character(),
+    version = paste0("v", projr_version_get()),
     hash = fn_to_hash_vec
   )
+}
+
+.projr_manifest_write <- function(manifest, output_run) {
+  if (!output_run) {
+    dir_cache_auto <- .projr_dir_get_cache_auto()
+    dir_write <- file.path(
+      dir_cache_auto, "projr", paste0("v", projr_version_get())
+    )
+    if (!dir.exists(dir_write)) {
+      dir.create(dir_write, recursive = TRUE)
+    }
+  } else {
+    dir_write <- projr_dir_get("project")
+  }
+  utils::write.csv(
+    manifest, file.path(dir_write, "manifest.csv"),
+    append = TRUE
+  )
+}
+
+.projr_manifest_read <- function() {
+  utils::read.csv(
+    projr_path_get("project", "manifest.csv"),
+    stringsAsFactors = FALSE
+  )
+}
+
+.projr_manifest_compare <- function(manifest_add, manifest2) {
+
 }
