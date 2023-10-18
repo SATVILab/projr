4783c5f58e00b29f2016daef2ed8c7d850090530
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-10-18 14:13:26 +0000

    Add working changes up until sub-sub OSF dest adds

---
+++ R/osf-yml-helper.R
@@ -0,0 +1,315 @@
+.projr_osf_yml_check_title <- function(title) {
+  if (is.null(title)) {
+    return(invisible(FALSE))
+  }
+  if (!length(title) == 1L) {
+    stop("title must be a character vector of length 1")
+  }
+  if (!is.character(title)) {
+    stop("title must be a character vector")
+  }
+  if (!nchar(title) > 0L) {
+    stop("title must have at least one character") # nolint
+  }
+}
+
+.projr_osf_yml_check_label <- function(label, type_opt) {
+  label_nn <- deparse(substitute(label))
+  # check supplied
+  if (missing(label)) {
+    stop(paste0(
+      label_nn, " must be supplied"
+    ))
+  }
+  if (is.null(label)) {
+    stop(paste0(
+      label_nn, " must be a character vector"
+    ))
+  }
+  # check it only matches restricted types
+  short_to_match_type <- c(
+    "cache" = "^cache",
+    "data-raw" = "^dataraw",
+    "output" = "^output",
+    "archive" = "^archive",
+    "docs" = "^docs"
+  )
+  match_str <- paste0(
+    short_to_match_type[type_opt],
+    collapse = "|"
+  )
+  if (!grepl(match_str, .projr_dir_label_strip(label))) {
+    stop(paste0(
+      label_nm, " entries must match the following: ",
+      match_str
+    ))
+  }
+
+  # check that it's actually found
+  label_vec <- unique(c(
+    names(projr_yml_get_unchecked()[["directories"]]),
+    "docs"
+  ))
+  if (!all(label %in% label_vec)) {
+    diff_vec <- setdiff(content, label_vec)
+    stop(paste0(
+      "content must match a directory key in _projr.yml. ",
+      "The following do not: ",
+      paste0('"', diff_vec, '"', collapse = ", ")
+    ))
+  }
+}
+.projr_osf_yml_check_public <- function(public) {
+  if (!is.logical(public)) {
+    stop("public must be logical")
+  }
+}
+.projr_osf_yml_check_body <- function(body) {
+  body_invalid <- (!is.null(body)) &&
+    (!all(is.character(body)) || length(body) != 1)
+  if (body_invalid) {
+    stop("body must be a character vector (with one element)")
+  }
+}
+.projr_osf_yml_check_category <- function(category) {
+  category_vec_valid <- c(
+    "analysis",
+    "communication",
+    "data",
+    "hypothesis",
+    "instrumentation",
+    "methods and measures",
+    "procedure",
+    "project",
+    "software",
+    "other"
+  )
+  if (is.null(category)) {
+    return(invisible(FALSE))
+  }
+  if (!length(category) == 1L) {
+    stop("category must be a character vector of length 1")
+  }
+  if (!is.character(category)) {
+    stop("category must be a character vector")
+  }
+  if (!all(category %in% category_vec_valid)) {
+    stop(paste0(
+      "category must be one of: ",
+      paste0('"', category_vec_valid, '"', collapse = ", ")
+    ))
+  }
+}
+.projr_osf_yml_check_id <- function(id) {
+  if (is.null(id)) {
+    return(invisible(FALSE))
+  }
+  if (!length(id) == 1L) {
+    stop("id must be a character vector of length 1")
+  }
+  if (!is.character(id)) {
+    stop("id must be a character vector")
+  }
+  if (!nchar(id) == 5L) {
+    stop("id must have five characters") # nolint
+  }
+}
+.projr_osf_yml_check_parent_id <- function(parent_id) {
+  if (is.null(parent_id)) {
+    return(invisible(FALSE))
+  }
+  if (!length(parent_id) == 1L) {
+    stop("parent_id must be a character vector of length 1")
+  }
+  if (!is.character(parent_id)) {
+    stop("parent_id must be a character vector")
+  }
+  if (!nchar(parent_id) == 5L) {
+    stop("parent_id must have five characters") # nolint
+  }
+}
+.projr_osf_yml_check_parent_title <- function(parent_title) {
+  if (is.null(parent_title)) {
+    return(invisible(FALSE))
+  }
+  if (!length(parent_title) == 1L) {
+    stop("parent_title must be a character vector of length 1")
+  }
+  if (!is.character(parent_title)) {
+    stop("parent_title must be a character vector")
+  }
+  if (!nchar(parent_title) > 0L) {
+    stop("parent_title must have at least one character") # nolint
+  }
+}
+.projr_osf_yml_check_proj_with_parent <- function(parent_id = NULL,
+                                                  parent_title = NULL,
+                                                  category) {
+  if (is.null(category)) {
+    return(invisible(FALSE))
+  }
+  if (category != "project") {
+    return(invisible(FALSE))
+  }
+
+  if (!is.null(parent_id) || !is.null(parent_title)) {
+    stop(paste0(
+      "parent_id and parent_title must be NULL when category is project"
+    ))
+  }
+}
+
+.projr_osf_yml_check_overwrite <- function(overwrite) {
+  if (!is.logical(overwrite)) {
+    stop("overwrite must be logical")
+  }
+}
+
+.projr_osf_yml_check_path <- function(path) {
+  if (is.null(path)) {
+    return(invisible(FALSE))
+  }
+  if (!length(path) == 1L) {
+    stop("path must be a character vector of length 1")
+  }
+  if (!is.character(path)) {
+    stop("path must be a character vector")
+  }
+  if (!nchar(path) > 0L) {
+    stop("path must have at least one character") # nolint
+  }
+}
+
+.projr_osf_yml_check_path_append_label <- function(path_append_label) {
+  if (is.null(path_append_label)) {
+    return(invisible(FALSE))
+  }
+  if (!length(path_append_label) == 1L) {
+    stop("path_append_label must be a logical vector of length 1")
+  }
+  if (!is.logical(path_append_label)) {
+    stop("path_append_label must be a logical vector")
+  }
+}
+.projr_osf_yml_check_remote_structure <- function(remote_structure,
+                                                  nm_opt = NULL) {
+  if (is.null(remote_structure)) {
+    return(invisible(FALSE))
+  }
+  if (is.null(nm_opt)) {
+    nm_opt <- c("latest", "version", "content")
+  }
+  diff_vec <- setdiff(remote_structure, nm_opt)
+  if (length(diff_vec) > 0) {
+    stop(paste0(
+      "remote_structure options must be one of: ",
+      paste0('"', nm_opt, '"', collapse = ", ")
+    ))
+  }
+}
+# transfer
+# ----------------------
+
+.projr_osf_yml_check_trans_list <- function(trans_list) {
+  direction <- deparse(substitute(trans_list))
+  if (!is.list(trans_list)) {
+    stop(paste0(direction, " options must be given as a list"))
+  }
+}
+
+.projr_osf_yml_check_trans_names <- function(trans_list,
+                                             nm_opt = NULL,
+                                             nm_opt_exc = NULL) {
+  if (is.null(nm_opt)) {
+    nm_opt <- c("cue", "sync-approach", "version-course", "conflict")
+  }
+  if (!is.null(nm_opt_exc)) {
+    nm_opt <- setdiff(nm_opt, nm_opt_exc)
+  }
+  direction <- deparse(substitute(trans_list))
+  diff_vec <- setdiff(names(trans_list), nm_opt)
+  if (length(diff_vec) > 0) {
+    stop(paste0(
+      direction,
+      " options must be one of: ",
+      paste0('"', nm_opt, '"', collapse = ", ")
+    ))
+  }
+}
+
+.projr_osf_yml_check_cue <- function(trans_list,
+                                     nm_opt = NULL) {
+  direction <- deparse(substitute(trans_list))
+  cue_vec <- trans_list[["cue"]]
+  if (is.null(cue_vec)) {
+    return(invisible(FALSE))
+  }
+  if (is.null(nm_opt)) {
+    nm_opt <- c("none", "build", "major", "minor", "patch")
+  }
+  diff_vec <- setdiff(cue_vec, nm_opt)
+  if (length(diff_vec) > 0) {
+    stop(paste0(
+      direction,
+      " cue options must be one of: ",
+      paste0('"', nm_opt, '"', collapse = ", ")
+    ))
+  }
+}
+
+.projr_osf_yml_check_sync_approach <- function(trans_list,
+                                               nm_opt = NULL) {
+  direction <- deparse(substitute(trans_list))
+  sync_approach_vec <- trans_list[["sync-approach"]]
+  if (is.null(sync_approach_vec)) {
+    return(invisible(FALSE))
+  }
+  if (is.null(nm_opt)) {
+    stop("nm_opt must be supplied for checking sync_approach")
+  }
+  diff_vec <- setdiff(sync_approach_vec, nm_opt)
+  if (length(diff_vec) > 0) {
+    stop(paste0(
+      direction,
+      " sync_approach options must be one of: ",
+      paste0('"', nm_opt, '"', collapse = ", ")
+    ))
+  }
+}
+
+.projr_osf_yml_check_conflict <- function(trans_list,
+                                          nm_opt = NULL) {
+  direction <- deparse(substitute(trans_list))
+  conflict_vec <- trans_list[["conflict"]]
+  if (is.null(nm_opt)) {
+    nm_opt <- c("error", "overwrite", "skip")
+  }
+  diff_vec <- setdiff(conflict_vec, nm_opt)
+  if (length(diff_vec) > 0) {
+    stop(paste0(
+      direction,
+      " conflict options must be one of: ",
+      paste0('"', nm_opt, '"', collapse = ", ")
+    ))
+  }
+}
+
+.projr_osf_yml_version_source <- function(trans_list,
+                                          nm_opt = NULL) {
+  direction <- deparse(substitute(trans_list))
+  version_source_vec <- trans_list[["version-source"]]
+  if (is.null(version_source_vec)) {
+    return(invisible(FALSE))
+  }
+  if (is.null(nm_opt)) {
+    nm_opt <- c("manifest", "osf")
+  }
+  diff_vec <- setdiff(version_source_vec, nm_opt)
+  if (length(diff_vec) > 0) {
+    stop(paste0(
+      direction,
+      " version_source options must be one of: ",
+      paste0('"', nm_opt, '"', collapse = ", ")
+    ))
+  }
+}
--- SCRATCH.R
+++ SCRATCH.R
@@ -3,13 +3,28 @@ library(testthat)
 devtools::load_all()
 # devtools::test_active_file("tests/testthat/test-osf-to_manual.R")
 devtools::test_active_file(
-  "tests/testthat/test-manual-osf-download.R"
+  "tests/testthat/test-osf-yml.R"
 )
 
 # scratch code
 yml_projr_dir <- projr_yml_get_unchecked()[["directories"]][["data-raw"]]
 
 # ==========================================
+
+# ==========================================
+# debugging projr_osf_dest_add
+# ==========================================
+
+browser()
+
+# debugonce(.projr_osf_yml_get_parent_vec)
+# debugonce(.projr_osf_yml_find_parent)
+# debugonce(.projr_osf_yml_find_parent_rec)
+# debugonce(projr_osf_dest_add)
+# debugonce(.projr_osf_yml_dest_add_comp_parent)
+browser()
+
+# debugonce(.projr_osf_yml_get_parent_vec)
 
 # =========================================
 # osf_download_to_dir_all
