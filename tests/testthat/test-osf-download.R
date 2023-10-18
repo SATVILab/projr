3ac21f1333b7f806c635871b81edb3b614f5c114
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-10-18 14:41:46 +0000

    Delete test no longer used

--- tests/testthat/test-osf-download.R
+++ tests/testthat/test-osf-download.R
@@ -1,5 +1,8 @@
-test_that(".projr_osf_get_path_dir_sub works", {
+test_that(".projr_checkout_osf works", {
   # skips
+  skip_if_offline()
+  skip_if(TRUE)
+
   # setup
   dir_test <- .projr_test_setup_project(git = TRUE, set_env_var = FALSE)
 
@@ -7,65 +10,121 @@ test_that(".projr_osf_get_path_dir_sub works", {
   usethis::with_project(
     path = dir_test,
     code = {
-      expect_null(.projr_osf_get_path_dir_sub(
-        label = "abc", path_append_label = FALSE, path = NULL
-      ))
-      expect_identical(
-        .projr_osf_get_path_dir_sub(
-          label = "abc", path_append_label = TRUE, path = NULL
-        ),
-        "abc"
-      )
-      expect_identical(
-        .projr_osf_get_path_dir_sub(
-          label = "abc", path_append_label = NULL, path = NULL
-        ),
-        "abc"
-      )
-      expect_identical(
-        .projr_osf_get_path_dir_sub(
-          label = "abc", path_append_label = TRUE, path = "def/ghi"
-        ),
-        "def/ghi/abc"
-      )
-      expect_identical(
-        .projr_osf_get_path_dir_sub(
-          label = "abc", path_append_label = FALSE, path = "def/ghi"
-        ),
-        "def/ghi"
-      )
-    },
-    quiet = TRUE,
-    force = TRUE
-  )
-})
+      # create previous upload
+      # ---------------------------
 
-test_that(".projr_osf_complete_dnld_list works", {
-  # skips
-  # setup
-  dir_test <- .projr_test_setup_project(git = TRUE, set_env_var = FALSE)
+      # create files
+      .projr_test_setup_content("data-raw")
 
-  # run from within project
-  usethis::with_project(
-    path = dir_test,
-    code = {
+      # create manifest
+      manifest <- .projr_test_manifest_create()
+
+      # create project
+      osf_tbl_proj <- .projr_test_osf_create_project("ProjectDownloadManifest")
+      .projr_osf_rm_node_id_defer(osf_tbl_proj[["id"]])
+
+      # upload to OSF
+      osf_tbl_upload <- osfr::osf_mkdir(x = osf_tbl_proj, path = "data-raw")
+      osf_tbl_dir <- with_dir(
+        "_data_raw",
+        {
+          .projr_osf_upload_dir(
+            osf_tbl = osf_tbl_upload, path_dir = "."
+          )
+        }
+      )
+      # add to YAML config
+      projr_osf_source_add(
+        label = "data-raw", id = osf_tbl_proj[["id"]]
+      )
+
+      # remove to test restore
+      unlink("_data_raw", recursive = TRUE)
+      dir.create("_data_raw")
+
+      # restore from a `latest` folder
+      # ------------------------
+      .projr_checkout_osf("data-raw")
+      # check files there
       expect_identical(
-        .projr_osf_complete_dnld_list(NULL),
-        list("conflict" = "overwrite", "sync-approach" = "download-all")
+        list.files(file.path("_data_raw")), c("abc.txt", "subdir1")
       )
       expect_identical(
-        .projr_osf_complete_dnld_list(list("conflict" = "abc")),
-        list("conflict" = "abc", "sync-approach" = "download-all")
+        list.files(file.path("_data_raw", "subdir1")), c("def.txt", "subdir2")
       )
       expect_identical(
-        .projr_osf_complete_dnld_list(
-          list("conflict" = "abc", "sync-approach" = "def")
-        ),
-        list("conflict" = "abc", "sync-approach" = "def")
+        list.files(file.path("_data_raw", "subdir1", "subdir2")), c("ghi.txt")
+      )
+
+      # restore from a versioned folder
+      # ------------------------
+      osfr::osf_rm(osf_tbl_upload, recurse = TRUE, check = FALSE)
+      osf_tbl_upload <- osfr::osf_mkdir(
+        x = osf_tbl_proj, path = "data-raw/v1.0.1"
+      )
+      osf_tbl_dir <- with_dir(
+        "_data_raw",
+        .projr_osf_upload_dir(osf_tbl = osf_tbl_upload, path_dir = ".")
+      )
+      # remove downloaded files
+      unlink("_data_raw", recursive = TRUE)
+      dir.create("_data_raw")
+
+      # add to YAML config
+      projr_osf_source_add(
+        label = "data-raw", id = osf_tbl_proj[["id"]],
+        remote_structure = "version", overwrite = TRUE
+      )
+
+      # remove to test restore
+      unlink("_data_raw", recursive = TRUE)
+      dir.create("_data_raw")
+      .projr_checkout_osf("data-raw", )
+      # check files there
+      expect_identical(
+        list.files(file.path("_data_raw")), c("abc.txt", "subdir1")
       )
       expect_identical(
-        .projr_osf_complete_dnld_list(list("sync-approach" = "def")),
-        list("conflict" = "overwrite", "sync-approach" = "def")
+        list.files(file.path("_data_raw", "subdir1")), c("def.txt", "subdir2")
+      )
+      expect_identical(
+        list.files(file.path("_data_raw", "subdir1", "subdir2")), c("ghi.txt")
+      )
+
+      # remove downloaded files
+      if (dir.exists("_data_raw")) unlink("_data_raw", recursive = TRUE)
+      dir.create("_data_raw")
+
+      # restore from a versioned folder but not the latest
+      # -----------------------------
+
+      # create files
+      .projr_test_setup_content("data-raw")
+      file.create("_data_raw/add.txt")
+
+      # add a new version
+      osf_tbl_upload <- osfr::osf_mkdir(
+        x = osf_tbl_proj, path = "data-raw/v2.0.1"
+      )
+      osf_tbl_dir <- with_dir(
+        "_data_raw",
+        .projr_osf_upload_dir(osf_tbl = osf_tbl_upload, path_dir = ".")
+      )
+
+      # remove downloaded files
+      unlink("_data_raw", recursive = TRUE)
+      dir.create("_data_raw")
+      # checkout
+      .projr_checkout_osf("data-raw", version = "1.5")
+      # check files there
+      expect_identical(
+        list.files(file.path("_data_raw")), c("abc.txt", "subdir1")
+      )
+      expect_identical(
+        list.files(file.path("_data_raw", "subdir1")), c("def.txt", "subdir2")
+      )
+      expect_identical(
+        list.files(file.path("_data_raw", "subdir1", "subdir2")), c("ghi.txt")
       )
     },
     quiet = TRUE,
