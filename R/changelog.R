5bc9c5ff8b5a32e59bfac8e8b3b517f723b7ad44
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2023-12-06 12:35:29 +0000

    Add changelog automation

---
+++ R/changelog.R
@@ -0,0 +1,70 @@
+.projr_build_changelog_add <- function(msg,
+                                       bump_component,
+                                       version_run_on_list) {
+  if (!.projr_build_changelog_check(bump_component)) {
+    return(invisible(FALSE))
+  }
+  .projr_changelog_add(msg, bump_component, version_run_on_list)
+}
+
+.projr_build_changelog_check <- function(bump_component) {
+  .projr_build_get_output_run(bump_component)
+}
+
+.projr_changelog_get <- function() {
+  path_changelog <- .projr_dir_proj_get("CHANGELOG.md")
+  if (!file.exists(path_changelog)) {
+    return(c("# CHANGELOG", ""))
+  }
+  readLines(path_changelog)
+}
+
+.projr_changelog_add <- function(msg, bump_component, version_run_on_list) {
+  init_txt <- c("# CHANGELOG", "")
+  add_txt <- .projr_changelog_get_add(msg, bump_component, version_run_on_list)
+  append_txt <- .projr_changelog_get()[-c(1, 2)]
+  c(init_txt, add_txt, append_txt) |>
+    .projr_changelog_write()
+  .projr_newline_append(.projr_dir_proj_get("CHANGELOG.md"))
+}
+
+.projr_changelog_get_add <- function(msg, bump_component, version_run_on_list) {
+  .projr_changelog_get_line(msg, bump_component, version_run_on_list) |>
+    .projr_changelog_get_entry(bump_component = bump_component)
+}
+
+
+.projr_changelog_get_line <- function(msg, bump_component, version_run_on_list) {
+  version_txt <- .projr_changelog_get_version(version_run_on_list)
+  bump_txt <- .projr_changelog_get_bump_component(bump_component)
+  .projr_change_get_line_final(bump_txt, version_txt, msg)
+}
+
+.projr_changelog_get_version <- function(version_run_on_list) {
+  paste0("v", version_run_on_list[["desc"]][["success"]])
+}
+
+.projr_changelog_get_bump_component <- function(bump_component) {
+  switch(bump_component,
+    "patch" = bump_component,
+    "minor" = paste0("*", bump_component, "*"),
+    "major" = paste0("**", bump_component, "**")
+  )
+}
+
+.projr_change_get_line_final <- function(bump, version, msg) {
+  paste0("- ", bump, " (", version, "): ", msg)
+}
+
+.projr_changelog_get_entry <- function(bump_component, line) {
+  switch(bump_component,
+    "patch" = line,
+    "minor" = line,
+    "major" = c(line, "")
+  )
+}
+
+.projr_changelog_write <- function(txt) {
+  path_changelog <- .projr_dir_proj_get("CHANGELOG.md")
+  writeLines(txt, path_changelog)
+}
