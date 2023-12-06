4527eccddf4b06a3ff46986ce41e3159d849e11d
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2023-12-06 14:55:19 +0000

    Add n_row options

--- R/cat.R
+++ R/cat.R
@@ -3,7 +3,9 @@
 #' @description
 #'  Cat useful information.
 #' `projr_cat_changelog`:
+#' @param n_row integer.
+#' Number of rows to cat.
 #' @export
-projr_cat_changelog <- function() {
-  .projr_changelog_read() |> cat()
+projr_cat_changelog <- function(n_row = 10) {
+  .projr_changelog_read()[seq_len(n_row)] |> cat()
 }
