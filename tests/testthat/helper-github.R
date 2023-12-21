324813dc783a0caa02f555bd5adc03228fda53f5
Darth Vader <number_one_fan@tellytubbies.com>
2023-12-21 20:26:31 +0000

    Add an excessive number of debug statements

--- tests/testthat/helper-github.R
+++ tests/testthat/helper-github.R
@@ -2,6 +2,7 @@
                                            token = NULL,
                                            repo = NULL,
                                            env = NULL) {
+  msssage("Beginning .projr_test_github_repo_create")
   # set up
   # ----------
   if (!requireNamespace("gh", quietly = TRUE)) {
@@ -18,6 +19,7 @@
   if (!requireNamespace("gh", quietly = TRUE)) {
     utils::install.packages("gh")
   }
+  message("p1")
   user <- user %||% gh::gh_whoami()[["login"]]
   if (!nzchar(user)) stop("No GitHub user found")
   token <- token %||% Sys.getenv("GITHUB_PAT")
@@ -27,14 +29,17 @@
   repo <- paste0("ProjrGitHubTest", repo, signif(stats::rnorm(1), 4))
 
   # check that it exists or not
+  message("p3")
   exists_ind <- .projr_test_github_repo_check_exists(
     user = user,
     token = token,
     repo = repo
   )
+  message("p4")
   if (exists_ind) {
     return(paste0(user, "/", repo))
   }
+  message("p5")
 
   # Define the URL of the GitHub API
   url <- "https://api.github.com/user/repos"
@@ -51,17 +56,20 @@
   # ----------
 
   # Make the POST request to the GitHub API
+  message("p6")
   response <- httr::POST(
     url,
     config = httr::add_headers(Authorization = paste("token", token)),
     body = body,
     encode = "json"
   )
+  message("p7")
 
   # check
   # ----------
 
   # Check the status of the response
+  message("p8")
   if (httr::http_status(response)$category == "Success") {
     file.create(file.path(.projr_test_git_remote_dir_get_tmp(), repo))
     # defer deletion
@@ -80,14 +88,18 @@
       envir = env
     )
     return(paste0(user, "/", repo))
+    message("p9")
   } else {
+    message("p10")
     character()
   }
+  message("Ending .projr_test_github_repo_create")
 }
 
 .projr_test_github_repo_check_exists <- function(user = NULL,
                                                  token = NULL,
                                                  repo = NULL) {
+  message("Beginning .projr_test_github_repo_check_exists")
   # set up
   # ----------
   if (!requireNamespace("gh", quietly = TRUE)) {
@@ -98,12 +110,14 @@
   }
 
   # defaults
+  message("p1")
   user <- user %||% gh::gh_whoami()[["login"]]
   if (!nzchar(user)) stop("No GitHub user found")
   token <- token %||% Sys.getenv("GITHUB_PAT")
   token <- if (!nzchar(token)) Sys.getenv("GH_TOKEN") else token
   if (!nzchar(token)) stop("No GitHub token found")
   repo <- repo %||% "test_projr_alt"
+  message("p2")
 
   # Define the URL of the GitHub API
   url <- paste0("https://api.github.com/repos/", user, "/", repo)
@@ -116,53 +130,73 @@
         Authorization = paste("token", token)
       )
   )
+  message("p3")
 
   # Check the status of the response
   httr::http_status(response)$category == "Success"
+  message("Ending .projr_test_github_repo_check_exists")
 }
 
 .projr_test_github_repo_remote_add <- function(user = NULL,
                                                token = NULL,
                                                repo = NULL) {
-  # Define the GitHub user and repository
+  message("Beginning .projr_test_github_repo_remote_add")
   if (!requireNamespace("gh", quietly = TRUE)) {
     utils::install.packages("gh")
   }
+  message("p1")
   if (!requireNamespace("httr", quietly = TRUE)) {
     utils::install.packages("httr")
   }
+  message("p2 ")
 
   # defaults
   user <- user %||% gh::gh_whoami()[["login"]]
+  message("p3")
   if (!nzchar(user)) stop("No GitHub user found")
+  message("p4")
   token <- token %||% Sys.getenv("GITHUB_PAT")
+  message("p5")
   token <- if (!nzchar(token)) Sys.getenv("GH_TOKEN") else token
+  message("p6")
   if (!nzchar(token)) stop("No GitHub token found")
+  message("p7")
   repo <- repo %||% "test_projr_alt"
+  message("p8")
 
   # Define the URL of the remote repository
+  message("p9")
   url <- paste0("https://github.com/", user, "/", repo, ".git")
 
   # Call the 'git' command to add the remote repository
+  message("p10")
   system2("git", args = c("remote", "add", "origin", url))
+  message("p11")
+  message("Ending .projr_test_github_repo_remote_add")
 }
 
 .projr_test_git_remote_get <- function() {
+  message("Beginning .projr_test_git_remote_get")
   system2("git", args = "remote", stdout = TRUE)
+  message("Ending .projr_test_git_remote_get")
 }
 
 .projr_test_git_branch_get <- function() {
+  message("Beginning .projr_test_git_branch_get")
   system2(
     "git",
     args = c("branch", "--show-current"), stdout = TRUE
   )
+  message("Ending .projr_test_git_branch_get")
 }
 
 .projr_test_git_set_upstream_and_force_push <- function(remote = NULL,
                                                         branch = NULL) {
+  message("Beginning .projr_test_git_set_upstream_and_force_push")
   remote <- remote %||% .projr_test_git_remote_get()
   remote <- remote[1]
   branch <- branch %||% .projr_test_git_branch_get()
+  message("p1")
   invisible(
     system2(
       "git",
@@ -170,6 +204,7 @@
       stdout = NULL, stderr = NULL
     )
   )
+  message("Ending .projr_test_git_set_upstream_and_force_push")
 }
 
 .projr_remote_host_rm_all_github <- function(user = NULL) {
