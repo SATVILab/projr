f53dc1ed4dec9b5e3b8c64f1fcfb6df382f94102
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2023-12-22 18:59:27 +0000

    Test authorisation functions

---
+++ tests/testthat/test-auth.R
@@ -0,0 +1,20 @@
+test_that(".projr_change_get_manifest works", {
+  # skip_if(.is_test_select())
+  dir_test <- .projr_test_setup_project(git = FALSE, set_env_var = TRUE)
+  usethis::with_project(
+    path = dir_test,
+    code = {
+      expect_true(.is_string(.projr_auth_get_github_pat()))
+      pat_old <- Sys.unsetenv("GITHUB_PAT")
+      .projr_auth_get_github_pat()
+      Sys.setenv("GITHUB_PAT" = pat_old)
+      suppressWarnings(.projr_auth_get_github_pat_warn())
+      suppressWarnings(.projr_auth_get_osf_pat())
+      pat_old <- Sys.unsetenv("OSF_PAT")
+      suppressWarnings(.projr_auth_get_osf_pat())
+      Sys.setenv("OSF_PAT" = pat_old)
+      suppressMessages(projr_instr_auth_github())
+      suppressMessages(projr_instr_auth_osf())
+    }
+  )
+})
