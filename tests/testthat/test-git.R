e4c612ce704e84d5a537d979c3a3e41ec9ef3b51
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2023-12-22 12:34:04 +0000

    Only run the second test-git.R test

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -1,6 +1,7 @@
 test_that(".projr_yml_git_ functions work", {
   # setup
   skip_if(.is_test_select())
+  skip()
   dir_test <- .projr_test_setup_project(git = FALSE, set_env_var = FALSE)
 
   # run from within project
@@ -62,7 +63,6 @@ test_that(".projr_yml_git_ functions work", {
 
 test_that(".projr_git_ functions work", { # setup
   skip_if(.is_test_select())
-  skip()
   dir_test <- .projr_test_setup_project(git = FALSE, set_env_var = FALSE)
 
   # run from within project
@@ -128,6 +128,7 @@ test_that(".projr_git_ functions work", { # setup
 
 test_that(".projr_git_ functions work", { # setup
   skip_if(.is_test_select())
+  skip()
   dir_test <- .projr_test_setup_project(
     git = TRUE, github = TRUE, set_env_var = TRUE
   )
