64b16693114c9af5861491e894f3a7e983bc1772
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2023-12-22 14:56:29 +0000

    Run until committing a file with gert

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -183,9 +183,9 @@ test_that(".projr_git_ functions work", { # setup
       print("done pushing a file with git")
       invisible(file.create("def.txt"))
       status_tbl <- gert::git_status()
-      skip()
       print("commit a file with gert")
       .projr_git_commit_file_gert("def.txt", msg = "def")
+      skip()
       print("done committing a file with gert")
       if (!Sys.getenv("GITHUB_ACTIONS") == "true") {
         expect_true(.projr_git_push_gert())
