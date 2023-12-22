5d817d95d91bee895b7fc1edebaca61890394bf0
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2023-12-22 13:36:05 +0000

    Test only second test again

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -61,7 +61,7 @@ test_that(".projr_yml_git_ functions work", {
 })
 
 test_that(".projr_git_ functions work", { # setup
-  skip_if(.is_test_select())
+  # skip_if(.is_test_select())
   dir_test <- .projr_test_setup_project(git = FALSE, set_env_var = FALSE)
 
   # run from within project
@@ -143,7 +143,7 @@ test_that(".projr_git_ functions work", { # setup
 
 
 test_that(".projr_git_ functions work", { # setup
-  # skip_if(.is_test_select())
+  skip_if(.is_test_select())
   dir_test <- .projr_test_setup_project(
     git = TRUE, github = TRUE, set_env_var = TRUE
   )
