9ec0c810f3a534e7e4ecefb2f6b5c5b5ff0f8787
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-01-15 08:26:35 +0000

    Solve authentication problem

--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -5,18 +5,19 @@ test_that(".projr_change_get_manifest works", {
     path = dir_test,
     code = {
       expect_true(.is_string(.projr_auth_get_github_pat()))
-      pat_old <- Sys.unsetenv("GITHUB_PAT")
+      pat_old <- Sys.getenv("GITHUB_PAT")
+      Sys.unsetenv("GITHUB_PAT")
       .projr_auth_get_github_pat()
       Sys.setenv("GITHUB_PAT" = pat_old)
       suppressWarnings(.projr_auth_get_github_pat_warn())
       suppressWarnings(.projr_auth_get_osf_pat())
-      pat_old <- Sys.unsetenv("OSF_PAT")
+      pat_old <- Sys.getenv("OSF_PAT")
+      Sys.unsetenv("OSF_PAT")
       suppressWarnings(.projr_auth_get_osf_pat())
       Sys.setenv("OSF_PAT" = pat_old)
       suppressMessages(projr_instr_auth_github())
       suppressMessages(projr_instr_auth_osf())
       suppressWarnings(.projr_auth_get_osf_pat_warn())
-      Sys.setenv("GITHUB_PAT" = pat_old)
     }
   )
 })
