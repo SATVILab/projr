b41bcd238fdf55ee631ebfcf18533819dc3a3cf9
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-01-22 08:10:47 +0000

    Add useful scratch code

--- SCRATCH.R
+++ SCRATCH.R
@@ -14,3 +14,7 @@ library(testthat)
 devtools::load_all()
 devtools::test_active_file("tests/testthat/test-yml.R")
 Sys.getenv("GITHUB_PAT")
+
+library(testthat)
+devtools::load_all()
+.projr_test_coverage()
