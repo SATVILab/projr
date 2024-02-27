7f7128a6614825ba2dad9756e79ce1cf8674f74b
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-02-27 12:50:49 +0000

    Catch warnings for testing authentication

--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -1,5 +1,5 @@
 test_that(".projr_change_get_manifest works", {
-  skip_if(.is_test_select())
+  # skip_if(.is_test_select())
   dir_test <- .projr_test_setup_project(git = FALSE, set_env_var = TRUE)
   usethis::with_project(
     path = dir_test,
@@ -7,17 +7,17 @@ test_that(".projr_change_get_manifest works", {
       expect_true(.is_string(.projr_auth_get_github_pat()))
       pat_old <- Sys.getenv("GITHUB_PAT")
       Sys.unsetenv("GITHUB_PAT")
-      .projr_auth_get_github_pat()
+      suppressWarnings(.projr_auth_get_github_pat())
       Sys.setenv("GITHUB_PAT" = pat_old)
       suppressWarnings(.projr_auth_get_github_pat_warn())
       suppressWarnings(.projr_auth_get_osf_pat())
       pat_old <- Sys.getenv("OSF_PAT")
       Sys.unsetenv("OSF_PAT")
-      suppressWarnings(.projr_auth_get_osf_pat())
+      expect_warning(.projr_auth_get_osf_pat())
       Sys.setenv("OSF_PAT" = pat_old)
       suppressMessages(projr_instr_auth_github())
       suppressMessages(projr_instr_auth_osf())
-      suppressWarnings(.projr_auth_get_osf_pat_warn())
+      expect_warning(.projr_auth_get_osf_pat_warn())
     }
   )
 })
