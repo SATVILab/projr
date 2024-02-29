14612f483c7b9420c70d5f79e0a3059ddc756ca4
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2024-02-29 13:25:44 +0000

    Uncomment skip

--- tests/testthat/test-plan.R
+++ tests/testthat/test-plan.R
@@ -214,7 +214,7 @@ test_that(".projr_dest_send_get_plan_detail works", {
 })
 
 test_that(".projr_plan_implement works", {
-  # skip_if(.is_test_select())
+  skip_if(.is_test_select())
   dir_test <- .projr_test_setup_project(git = FALSE, set_env_var = TRUE)
   usethis::with_project(
     path = dir_test,
