5aaac78f9f3858643caafee1a9d632c6d7705715
Miguel Rodo <rdxmig002@myuct.ac.za>
2025-01-28 12:25:09 +0000

    Move unused code out

--- R/dest-send.R
+++ R/dest-send.R
@@ -150,7 +150,7 @@
   force(title)
   .projr_yml_dest_get_title_complete(
     title, type, NULL, upload_github, upload_force
-    )[["cue"]] |>
+  )[["cue"]] |>
     .is_cue(bump_component)
 }
 
@@ -194,83 +194,3 @@
     c(nm_vec_output, nm_vec_raw, nm_vec_docs)
   }
 }
-
-
-# ================================
-# clear function
-# ================================
-
-# delete the remote if it's empty and it's versioned
-.projr_dest_send_label_clear <- function(strategy,
-                                         type,
-                                         remote,
-                                         structure) {
-  if (!.is_opt(strategy, "sync-using-deletion")) {
-    return(invisible(FALSE))
-  }
-  .projr_remote_rm_final_if_empty(type, remote, structure)
-}
-
-
-# ================================
-# update versioning files
-# ================================
-
-.projr_dest_send_label_versioning_update <- function(type,
-                                                     remote,
-                                                     label,
-                                                     plan_detail) {
-  .projr_dest_send_label_versioning_update_manifest(
-    type, remote, label, plan_detail
-  )
-  .projr_dest_send_label_versioning_update_version_file(
-    type, remote, label, plan_detail
-  )
-}
-
-.projr_dest_send_label_versioning_update_manifest <- function(type, 
-                                                              remote, 
-                                                              label, 
-                                                              plan_detail) {
-  manifest_remote <- .projr_remote_get_manifest(type, remote)
-  manifest_project <- .projr_manifest_read_project()
-  manifest_add <- manifest_project |>
-    .projr_manifest_filter_label(label) |>
-    .projr_manifest_filter_version(projr::projr_version_get())
-  manifest_final <-
-    .projr_manifest_append_previous_actual(manifest_add, manifest_remote) |>
-    .projr_manifest_remove_duplicate()
-  .projr_remote_write_manifest(type, remote, manifest_final)
-  invisible(TRUE)
-}
-
-.projr_dest_send_label_versioning_update_check_skip <- function(plan_detail) {
-  .is_len_0(plan_detail[["add"]]) && .is_len_0(plan_detail[["rm"]])
-}
-
-.projr_dest_send_label_versioning_update_version_file <- function(type,
-                                                                  remote,
-                                                                  label,
-                                                                  plan_detail) {
-  version_file_remote <- .projr_remote_get_version_file(type, remote)
-  version_file <- .projr_version_file_update_project_version(
-    version_file_remote
-  )
-  check_skip_update_label <-
-    .projr_dest_send_label_versioning_update_check_skip_version_file(
-      plan_detail, version_file, label
-    )
-  if (!check_skip_update_label) {
-    version_file <- .projr_version_file_update_label_version(
-      version_file, label
-    )
-  }
-  .projr_remote_write_version_file(type, remote, version_file)
-}
-
-.projr_dest_send_label_versioning_update_check_skip_version_file <-
-  function(plan_detail, version_file, label) {
-    .is_len_0(plan_detail[["add"]]) && .is_len_0(plan_detail[["rm"]]) &&
-      any(grepl(paste0("^", label, ": "), version_file))
-}
-
