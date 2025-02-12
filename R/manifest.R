65d6ef441e553a1d8f7aca65eb34c8deacb88c23
Miguel Rodo <rdxmig002@myuct.ac.za>
2025-02-12 18:08:55 +0000

    Correct functino

--- R/manifest.R
+++ R/manifest.R
@@ -122,7 +122,8 @@
   invisible(path)
 }
 
-.projr_restore_label <- function(path = NULL) {
+# .projr_manifest_read <- function(path = NULL) {
+.projr_manifest_read <- function(path = NULL) {
   if (!.is_string(path) || !file.exists(path)) {
     return(.projr_zero_tbl_get_manifest())
   }
