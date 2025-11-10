5a0363274669d9499699ef05032dac2ceef53737
Miguel Julio Rodo <miguel.rodo@uct.ac.za>
2025-11-10 07:26:13 +0000

    Bump renv version from 1.1.4 to 1.1.5 in activate.R

--- _dependencies.R
+++ _dependencies.R
@@ -1,3 +1,5 @@
-library(tinytest)
-library(BiocGenerics)
-library(BiocVersion)
+pkg_vec <- c("bookdown", "cffr", "codemeta", "covr", "credentials",
+             "devtools", "DT", "gert", "htmltools", "knitr", "osfr",
+             "pkgbuild", "pkgdown", "quarto", "remotes", "rmarkdown",
+             "roxygen2", "testthat", "usethis")
+renv::install(pkg_vec)
